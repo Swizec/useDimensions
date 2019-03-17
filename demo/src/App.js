c537f99ef6920027de9b3947dbffe93d7ec98d68
Swizec Teller <swizec@swizec.com>
2019-03-17 15:05:18 -0700

    Create demo, unsuccessful in solving core issue

---
+++ demo/src/App.js
@@ -0,0 +1,102 @@
+import React, { Component, useState } from "react";
+import "./App.css";
+import useDimensions from "../../src/index";
+
+const MeasuredNode = () => {
+    const [ref, { x, y, width, height }] = useDimensions();
+
+    return (
+        <p ref={ref}>
+            I am a paragraph at ({x}px, {y}px) position with a width of {width}
+            px and height of {height}px
+        </p>
+    );
+};
+
+const MyComponent = () => {
+    const [ref, { width }] = useDimensions();
+    return (
+        <Parent childRef={ref}>
+            <p>Hello is {width}px wide</p>
+        </Parent>
+    );
+};
+
+const Parent = ({ childRef, children }) => {
+    const [showChild, setShowChild] = useState(false);
+    return (
+        <React.Fragment>
+            {showChild ? <div ref={childRef}>Hello</div> : null}
+            <button onClick={() => setShowChild(!showChild)}>
+                Toggle Hello
+            </button>
+            {children}
+        </React.Fragment>
+    );
+};
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App App-header">
+                <h1>useDimensions demo</h1>
+                <p>
+                    <a href="https://github.com/Swizec/useDimensions">
+                        useDimensions
+                    </a>{" "}
+                    is a React Hook for measuring DOM nodes. You can use it to
+                    measure any element in your DOM. Like this 👇
+                </p>
+                <code>
+                    <pre>{`
+import React from 'react'
+import useDimensions from 'react-use-dimensions'
+
+const MeasuredNode = () => {
+    const [ref, { x, y, width, height }] = useDimensions();
+
+    return (
+        <p ref={ref}>
+            I am a paragraph at ({x}px, {y}px) position with a width of {width}px
+            and height of {height}px
+        </p>
+    );
+};
+`}</pre>
+                </code>
+                <p>👇</p>
+                <MeasuredNode />
+                <h2>Works with deep nested components</h2>
+                <MyComponent />
+                <p>👆</p>
+                <code>
+                    <pre>{`
+const MyComponent = () => {
+    const [ref, { width }] = useDimensions();
+    return (
+        <Parent childRef={ref}>
+            <p>Hello is {width}px wide</p>
+        </Parent>
+    );
+};
+
+const Parent = ({ childRef, children }) => {
+    const [showChild, setShowChild] = useState(false);
+    return (
+        <React.Fragment>
+            {showChild ? <div ref={childRef}>Hello</div> : null}
+            <button onClick={() => setShowChild(!showChild)}>
+                Toggle Hello
+            </button>
+            {children}
+        </React.Fragment>
+    );
+};
+                `}</pre>
+                </code>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { useRef, useState, useLayoutEffect } from "react";
 
 function useDimensions() {
-    const ref = useRef();
+    const ref = useRef(); // replacing this with ref callback, also creates infinite loops
     const [dimensions, setDimensions] = useState({});
     useLayoutEffect(() => {
         ref.current &&
             setDimensions(ref.current.getBoundingClientRect().toJSON());
-    }, [ref.current]);
+    }, [ref.current]); // removing this creates infinite loops
 
     return [ref, dimensions];
 }
