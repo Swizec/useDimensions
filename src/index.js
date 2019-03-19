1daf8995137dae66c3285b47ff32c406078ec320
Swizec Teller <swizec@swizec.com>
2019-03-18 23:19:29 -0700

    Fix Microsoft Edge support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import { useRef, useState, useLayoutEffect } from "react";
 
+function getDimensionObject(ref) {
+    const rect = ref.current.getBoundingClientRect();
+
+    if (rect.toJSON) {
+        return rect.toJSON();
+    } else {
+        return {
+            width: rect.width,
+            height: rect.height,
+            top: rect.top || rect.x,
+            left: rect.left || rect.left,
+            x: rect.x || rect.left,
+            y: rect.y || rect.top,
+            right: rect.right,
+            bottom: rect.bottom
+        };
+    }
+}
+
 function useDimensions() {
     const ref = useRef(); // replacing this with ref callback, also creates infinite loops
     const [dimensions, setDimensions] = useState({});
     useLayoutEffect(() => {
-        ref.current &&
-            setDimensions(ref.current.getBoundingClientRect().toJSON());
+        ref.current && setDimensions(getDimensionObject(ref));
     }, [ref.current]); // removing this creates infinite loops
 
     return [ref, dimensions];
