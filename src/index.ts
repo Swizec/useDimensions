8ba43a230340766f21ae0b36234d133b4764f415
János Orcsik <orcsikj@gmail.com>
2019-06-17 14:04:06 +0200

    Fix top & left calculation

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ function getDimensionObject(node: HTMLElement): DimensionObject {
     return {
         width: rect.width,
         height: rect.height,
-        top: "x" in rect ? rect.x : rect.top,
-        left: "y" in rect ? rect.y : rect.left,
+        top: "y" in rect ? rect.y : rect.top,
+        left: "x" in rect ? rect.x : rect.left,
         x: "x" in rect ? rect.x : rect.left,
         y: "y" in rect ? rect.y : rect.top,
         right: rect.right,
